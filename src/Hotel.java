ea0358d4e48c1bb45b943367c52697b36e6024b8
azuressu <martina0130@naver.com>
2023-06-05 16:42:08 +0900

    second commit

--- src/Hotel.java
+++ src/Hotel.java
@@ -12,6 +12,7 @@ public class Hotel {
 
     // 객실 리스트 출력하는 메소드
     // 예약하고 싶은 날짜를 입력 받기
+    // 형철님
     public void showRoomList() {
         // 오늘 날짜 출력
         // 고객으로부터 원하는 날짜 입력받기
@@ -21,6 +22,7 @@ public class Hotel {
 
     // 객실 입력받는 메소드
     // 입력받아서 다음 메소드인 고객 정보를 입력 받을 때 Room 타입의 필드를 넘겨줌
+    // 병민님
     public Room inputRoom() {
         Room room = null;
 
@@ -30,6 +32,7 @@ public class Hotel {
     // 고객 정보를 입력받는 메소드
     // 고객 정보를 입력 받아서 있으면 그냥 진행하고, 없으면 고객 리스트에 추가하기
     // 그 다음에 소지금 판단하기
+    // 인서님
     public Customer inputCustomer() {
         Customer customer = null;
 
@@ -39,6 +42,7 @@ public class Hotel {
     // 예약 완료 화면
     // 예약 번호를 반환 (uuid)
     // 예약 리스트 안에 Reservation (객실과 고객이름과 고객전화번호, 예약날짜, 예약번호) 저장하기
+    // 푸름님
     public void reservationComplete() {
 
     }
@@ -47,12 +51,13 @@ public class Hotel {
     // 이름만 전화번호를 입력받고 고객 리스트에서 판별하기
     // 고객 리스트에 있으면 예약 목록에서 출력해줌
     // 삭제 여부를 입력받고, 예약 번호 입력받아서 맞으면 삭제
-
+    // 수연
     public void confirmDelete() {
 
     }
 
     // 호텔에서 모든 예약 목록 조회 기능
+    // 푸름님
     public void showAllReservation() {
 
     }
